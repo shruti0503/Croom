82c751e0d71b8cad385b9dae0409e739a402d1cd
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-05-05 19:54:08 +0530

    Update page.tsx

--- my-app/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ my-app/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,12 @@
-
+'use client'
 import { SignIn } from '@clerk/nextjs'
-// import { useEffect } from 'react'
+import { useEffect } from 'react'
 
 function SignInPage() {
-  // useEffect(()=>{
-  //   console.log("signin page")
+  useEffect(()=>{
+    console.log("signin page")
 
-  // },[])
+  },[])
   console.log("signin page")
   return (
     <div className='flex h-screen w-full items-center justify-center'><SignIn /></div>
