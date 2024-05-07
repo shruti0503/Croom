1ea03cf2335908fe80f0adf137db0fa776d4fa3b
shrutiVishwakarma05 <shrutivishwakarma0509@gmail.com>
2024-05-07 22:27:05 +0530

    updated provider

--- my-app/providers/StreamClientProvider.tsx
+++ my-app/providers/StreamClientProvider.tsx
@@ -1,56 +1,38 @@
-'use client'
+'use client';
+
+import { ReactNode, useEffect, useState } from 'react';
+import { StreamVideoClient, StreamVideo } from '@stream-io/video-react-sdk';
 import { useUser } from '@clerk/nextjs';
-import {
-    StreamVideo,
-    StreamVideoClient,
-    User,
-  } from '@stream-io/video-react-sdk';
-  import { ReactNode, useEffect } from 'react';
-  import { useState } from 'react';
-  import { tokenProvider } from '@/actions/stream.actions';
-  import Loader from '@/components/ui/Loader';
-  
-  // next public means -> exposed to the lcient side of the application 
-  const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
+import { tokenProvider } from '@/actions/stream.actions';
+import Loader from '@/components/ui/Loader';
 
+const API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
 
-   const StreamVideoProvider = ({children}:{ children: ReactNode }) => {
+const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
+  const [videoClient, setVideoClient] = useState<StreamVideoClient>();
+  const { user, isLoaded } = useUser();
 
-    const [videoClient, setVideoClient]=useState<StreamVideoClient>()
-    const {user, isLoaded}=useUser();
-//clerk cvgeckl
-    useEffect(()=>{
-        if(!isLoaded || !user) return;
-        if(!apiKey) throw new Error('Stream API key missing')
+  useEffect(() => {
+    if (!isLoaded || !user) return;
+    if (!API_KEY) throw new Error('Stream API key is missing');
 
-        const client=new StreamVideoClient({
-            apiKey,
-            user:{
-                id:user?.id,
-                name:user?.username || user?.id,
-                image:user?.imageUrl,
-            },
-            tokenProvider:tokenProvider
-        })
-        setVideoClient(client)
+    const client = new StreamVideoClient({
+      apiKey: API_KEY,
+      user: {
+        id: user?.id,
+        name: user?.username || user?.id,
+        image: user?.imageUrl,
+      },
+      tokenProvider,
+    });
 
-     },[user, isLoaded]);
+    setVideoClient(client);
+  }, [user, isLoaded]);
 
-     if(!videoClient) return <Loader />
-   
-    
+  if (!videoClient) return <Loader />;
 
-    return(
-        <StreamVideo 
-        //@ts-ignore
-        client={videoClient}
-        >
-            {children}
-        </StreamVideo>
+  return <StreamVideo client={videoClient}>{children}</StreamVideo>;
+};
 
-    )
-     
-  };
-
-  export default StreamVideoProvider
+export default StreamVideoProvider;
